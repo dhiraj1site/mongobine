0a39222e2a56af4329f69fe56d4a4e01c4a0cf74
Dhiraj <dhiraj201site@gmail.com>
2021-06-29 13:48:23 -0400

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var mongo = new mongobine({collection: 'users', skipTimeStamp: false});
 //CRUD operations for mongodb
 
 //create
-mongo.insert({email: "testinfinal"});
+mongo.insert({email: "myemail@email.com"});
 
 // //retrieve
 mongo.find({});
